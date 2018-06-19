95938665dd26804ea5279fe556c0a7a3901686ab
OY <objectyan@gmail.com>
2018-06-19 16:34:35 +0800

    update

---
+++ android/NerdLauncher/app/src/main/java/me/objectyan/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package me.objectyan.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
---
+++ android/PhotoGallery/app/src/main/java/me/objectyan/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,21 @@
+package me.objectyan.photogallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        StrictMode.enableDefaults();
+        return PhotoGalleryFragment.newInstance();
+    }
+}
